c6c641b65953babb0f626759c54039cd7d7600ab
Delpi <tungvuit@gmail.com>
2025-10-08 16:28:59 +0700

    chore: default footer is false

--- build/calendar/index.d.ts
+++ build/calendar/index.d.ts
@@ -1,6 +1,4 @@
 import React from "react";
 import type { ICalendarRefProps, IDatePickerProps } from "./types";
-import "react-dropdown-z/build/styles.css";
-import "./styles.scss";
 declare const Calendar: React.ForwardRefExoticComponent<IDatePickerProps & React.RefAttributes<ICalendarRefProps>>;
 export default Calendar;
